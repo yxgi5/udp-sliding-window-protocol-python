f596193d4746ea570d3da3ad51d35d542a445096
Andreas Zhang <denglitsch@gmail.com>
2019-01-03 22:50:52 +0800

    tsUserv.py on pi, tsUclnt.py on pc

--- tsUclnt.py
+++ tsUclnt.py
@@ -3,21 +3,29 @@
 
 from socket import *
 
-HOST = '127.0.0.1'
-PORT = 21567
+#TGT_HOST = '127.0.0.1'
+TGT_HOST = '192.168.0.103'
+'''
+pc:192.168.0.102 ##
+pi:192.168.0.103
+'''
+TGT_PORT = 21567
 BUFSIZ = 1024
-ADDR = (HOST, PORT)
+TGT_ADDR = (TGT_HOST, TGT_PORT)
+LC_HOST = ''
+LC_PORT = 21568
+LC_ADDR = (LC_HOST, LC_PORT)
 
-udpCliSock = socket(AF_INET, SOCK_DGRAM)
-
+udpSock = socket(AF_INET, SOCK_DGRAM)
+udpSock.bind(LC_ADDR)
 while True:
-    data = input('> ')
-    if not data:
+    send_data = input('> ')
+    if not send_data:
         break
-    udpCliSock.sendto(data.encode(), ADDR)
-    data, ADDR = udpCliSock.recvfrom(BUFSIZ)
-    if not data:
+    udpSock.sendto(send_data.encode(), TGT_ADDR)
+    rcv_data, RCV_ADDR = udpSock.recvfrom(BUFSIZ)
+    if not rcv_data:
         break
-    print(data.decode())
+    print(rcv_data.decode())
 
-udpCliSock.close()
+udpSock.close()
--- tsUserv.py
+++ tsUserv.py
@@ -4,18 +4,26 @@
 from socket import *
 from time import ctime
 
-HOST = ''
-PORT = 21567
+#TGT_HOST = '127.0.0.1'
+TGT_HOST = '192.168.0.102'
+'''
+pc:192.168.0.102
+pi:192.168.0.103 ##
+'''
+TGT_PORT = 21568
 BUFSIZ = 1024
-ADDR = (HOST, PORT)
+TGT_ADDR = (TGT_HOST, TGT_PORT)
+LC_HOST = ''
+LC_PORT = 21567
+LC_ADDR = (LC_HOST, LC_PORT)
 
-udpSerSock = socket(AF_INET, SOCK_DGRAM)
-udpSerSock.bind(ADDR)
+udpSock = socket(AF_INET, SOCK_DGRAM)
+udpSock.bind(LC_ADDR)
 
 while True:
     print('waiting for message...')
-    data, addr = udpSerSock.recvfrom(BUFSIZ)
-    udpSerSock.sendto(('[%s] %s' % (ctime(), data.decode())).encode(), addr)
-    print(data.decode(), 'received from and returned to:', addr)
+    data, addr = udpSock.recvfrom(BUFSIZ)
+    udpSock.sendto(('[%s] %s' % (ctime(), data.decode())).encode(), TGT_ADDR)
+    print(data.decode(), 'received from and returned to:', TGT_ADDR)
 
-udpSerSock.close()
+udpSock.close()
